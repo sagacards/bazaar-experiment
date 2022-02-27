4bc770a7a35f5dc848d5ef9e172af564ba1f7692
Jorgen <jorgen@buildnode.io>
2022-02-26 17:32:21 -0800

    more polyfill

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import NodeGlobalsPolyfillPlugin from '@esbuild-plugins/node-globals-polyfill'
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,11 +14,19 @@ export default defineConfig({
     esbuildOptions: {
       plugins: [
         NodeGlobalsPolyfillPlugin({
-          process: true,
           buffer: true,
           define: { 'process.env.NODE_ENV': '"production"' }, // https://github.com/evanw/esbuild/issues/660
       }),
       ]
     }
-  }
+  },
+  build: {
+    rollupOptions: {
+        plugins: [
+            // Enable rollup polyfills plugin
+            // used during production bundling
+            rollupNodePolyFill()
+        ]
+    }
+}
 })
